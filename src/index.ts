c3761e7f1b7eed09d2e3c182c8dfb354ea9eb308
Anshul Kamath <anshulkam@gmail.com>
2023-03-25 19:01:14 -0700

    added static folders

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ const port = process.env.PORT ?? 8080
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('src/frontend'))
 
 app.get('/puzzle', getPuzzleHandler)
 app.get('/spotify', getSpotifySong)
