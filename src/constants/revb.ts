ac5b00420c3734556c7a81eb5a16fd4d74abbc05
Anshul Kamath <anshulkam@gmail.com>
2023-06-10 14:47:13 -0400

    minor updates to revb

--- src/constants/revb.ts
+++ src/constants/revb.ts
@@ -1,5 +1,5 @@
 const DEFAULT_MAX_ROUNDS = 10
-const DEFAULT_SCORE_THRESH = 930
+const DEFAULT_SCORE_THRESH = 900
 
 export const MAX_ROUNDS = parseInt(process.env.MAX_ROUNDS ?? `${DEFAULT_MAX_ROUNDS}`)
 export const SCORE_THRESH = parseInt(process.env.SCORE_THRESH ?? `${DEFAULT_SCORE_THRESH}`)
